8217bd7a6e994897a1d685ae54dc4d54e6c4e3c3
ester <95254487+esterrpereira@users.noreply.github.com>
2022-04-21 15:11:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-   /*
+  /*
 Josefá é uma feirante que possui em sua banca 05 produtos com os
 seguintes preços: tomate R$2,30; cebola R$1,20; cenoura R$ 3,70; abobrinha R$
 5,10; brócolis R$ 1,23. Desenvolva um programa que possibilite ao usuários as
@@ -15,95 +15,71 @@ ele escolher qual ele vai alterar);
 */
 
   int opcao, codigo, produto;
-  float preco;
-// while para o loop do menu 
-while (opcao ) {
-   printf(
-      "\n=================================================\n\n\t\tMENU PRINCIPAL\n\n\t1-LISTAR O NOME DOS PRODUTOS\n\t2-LISTAR "
-      "O NOME E O PREÇO DOS PRODUTOS\n\t3-ALTERAR O PREÇO DOS "
-      "PRODUTOS\n\n=================================================\n\t "
-      "Digite a opção ou 0 para parar: ");
-       scanf("%d", &opcao);
-  
-  
-  // switch-case para escolha
-  switch (opcao) {
-  case 0:
-    printf("Escolheu sair do programa");
-    break;
-  case 1:
-    printf("\n\n\t\t1-TOMATE\n\t\t2-CEBOLA\n\t\t3-CENOURA\n\t\t4-"
-           "ABOBRINHA\n\t\t5-BRÓCOLIS\n");
-    break;
-  case 2:
-    printf("\n\n\t\t1-TOMATE\t\tR$2,30\n\t\t2-CEBOLA\t\tR$1,20\n\t\t3-"
-           "CENOURA\t\tR$3,70\n\t\t4-ABOBRINHA\t\tR$5,10\n\t\t5-BRÓCOLIS\t\tR$"
-           "1,23\n");
-    break;
-  case 3:
-    printf("\n\n\t\t[1]\t\tTOMATE\n\t\t[2]\t\tCEBOLA\n\t\t[3]\t\tCENOURA\n\t\t[4]\t\t"
-           "ABOBRINHA\n\t\t[5]\t\tBRÓCOLIS\n\n======================================"
-           "===========\nDigite o código do produto que deseja alterar: ");
-    scanf("%d", &codigo);
+  float preco, alter = preco;
+  // while para o loop do menu
+  while (opcao) {
+    printf("\n=================================================\n\n\t\tMENU "
+           "PRINCIPAL\n\n\t1-LISTAR O NOME DOS PRODUTOS\n\t2-LISTAR "
+           "O NOME E O PREÇO DOS PRODUTOS\n\t3-ALTERAR O PREÇO DO "
+           "PRODUTOS\n\n=================================================\n\t "
+           "Digite a opção ou 0 para parar: ");
+    scanf("%d", &opcao);
 
-    break;
-  default:
-    printf("\n\n\t\tERRO\n");
-  }
-  // switch - case para a leitura da varíavel "
-
-  switch (codigo) {
-  case 1:
-    printf("(1) Digite o novo preço: ");
-    scanf("%f", &preco);
-    // CONFIRMAÇÃO
-    while (preco) {
-      printf("1-Alteração feita com sucesso");
+    // switch-case para escolha
+    switch (opcao) {
+    case 0:
+      printf("Escolheu sair do programa");
       break;
-    }
-
-    break;
-  case 2:
-    printf("(2) Digite o novo preço: ");
-    scanf("%f", &preco);
-    // CONFIRMAÇÃO
-    while (preco) {
-      printf("2-Alteração feita com sucesso");
+    case 1:
+      printf("\n\n\t\t1-TOMATE\n\t\t2-CEBOLA\n\t\t3-CENOURA\n\t\t4-"
+             "ABOBRINHA\n\t\t5-BRÓCOLIS\n");
       break;
-    }
-
-    break;
-  case 3:
-    printf("(3) Digite o novo preço: ");
-    scanf("%f", &preco);
-    // CONFIRMAÇÃO
-    while (preco) {
-      printf("3-Alteração feita com sucesso");
+    case 2:
+      printf(
+          "\n\n\t\t1-TOMATE\t\tR$2,30\n\t\t2-CEBOLA\t\tR$1,20\n\t\t3-"
+          "CENOURA\t\tR$3,70\n\t\t4-ABOBRINHA\t\tR$5,10\n\t\t5-BRÓCOLIS\t\tR$"
+          "1,23\n");
       break;
-    }
-    break;
+    case 3: {
 
-  case 4:
-    printf("(4) Digite o novo preço: ");
-    scanf("%f", &preco);
-    // CONFIRMAÇÃO
-    while (preco) {
-      printf("4-Alteração feita com sucesso");
+      printf("\n\n\t\t[1]\t\tTOMATE\n\t\t[2]\t\tCEBOLA\n\t\t[3]"
+             "\t\tCENOURA\n\t\t[4]\t\t"
+             "ABOBRINHA\n\t\t[5]\t\tBRÓCOLIS\n\n==============================="
+             "======="
+             "===========\nDigite o código do produto que deseja alterar: ");
+      scanf("%d", &codigo);
+
+      // switch - case para a leitura da varíavel "codigo"
+
+      switch (codigo) {
+      case 1:
+        printf("(1) Digite o novo preço: ");
+        scanf("%f", &preco);
+        break;
+      case 2:
+        printf("(2) Digite o novo preço: ");
+        scanf("%f", &preco);
+        break;
+      case 3:
+        printf("(3) Digite o novo preço: ");
+        scanf("%f", &preco);
+        break;
+      case 4:
+        printf("(4) Digite o novo preço: ");
+        scanf("%f", &preco);
+        break;
+      case 5:
+        printf("(5) Digite o novo preço: ");
+        scanf("%f", &preco);
+        break;
+      }
+      // CONFIRMAÇÃO DE ALTERAÇÃO E LEITURA DA VARÍAVEL "alter"
+      printf("ALTERAÇÃO FEITA COM SUCESSO");
       break;
+      scanf("%f", &alter);
     }
-    break;
-
-  case 5:
-    printf("(5) Digite o novo preço: ");
-    scanf("%f", &preco);
-    // CONFIRMAÇÃO
-    while (preco) {
-      printf("5-Alteração feita com sucesso");
-      break;
+    default:
+      printf("\n\n\t\tERRO\n");
     }
-
-    break;
   }
 }
-}
-
